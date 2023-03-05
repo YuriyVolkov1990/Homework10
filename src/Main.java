147f4b7be5df4c8d5db36132b9a5981d642d9a2a
Professional <new99kpicem@gmail.com>
2023-03-05 05:52:18 +0300

    studying

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phone = "111632127";
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        }
+        System.out.println("phone = " + phone);
+
     }
 }
