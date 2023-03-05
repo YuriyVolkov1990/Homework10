e70c9e04f349c2ca037dc80f9e98083d8eb711eb
Professional <new99kpicem@gmail.com>
2023-03-05 23:29:02 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
         //task3();
     }
     public static void task1() {
@@ -11,5 +11,13 @@ public class Main {
         String lastName = "Ivanov";
         String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName.toUpperCase());
     }
 }
